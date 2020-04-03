6f50a1c3381db9e3ec29f3ae2d651d44602d46da
Dannie Schumaker <draeschumaker@gmail.com>
2020-04-02 18:50:47 -0700

    write failing test for route that creates a meme

---
+++ __tests__/memes.test.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+require('../db/data-helpers'); 
+
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('meme routes', () => {
+  it('creates a meme', () => {
+    return request(app)
+      .post('/api/v1/memes')
+      .send({
+        top: 'some string',
+        image: 'a url',
+        bottom: 'the other half of the meme string'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          top: 'some string',
+          image: 'a url',
+          bottom: 'the other half of the meme string'
+        });
+      });
+  });
+});
